558aaf8c3d7baaec4247030d1cb3ef453c10bec2
miocid152 <lomen_ro_152@hotmail.com>
2014-11-23 15:10:52 -0600

    DB Actualizada

--- ServicioWeb/web/DB/soa.sql
+++ ServicioWeb/web/DB/soa.sql
@@ -19,7 +19,8 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `servicioweb`
 --
-
+CREATE SCHEMA IF NOT EXISTS `ServicioWeb` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
+USE `ServicioWeb` ;
 -- --------------------------------------------------------
 
 --
